bd0cc2b16ec359659c01718074b57e277edc50cf
burdms <burdms@gmail.com>
2020-12-16 19:55:59 +0100

    create: hero main & tablet responsive

--- js/main.js
+++ js/main.js
@@ -1,7 +1,20 @@
 $(document).ready(function () {
+  // Burger menu
   var menuButton = document.querySelector(".burger-button");
   menuButton.addEventListener("click", function () {
     document.querySelector(".menu").classList.toggle("menu_visible");
     document.querySelector("body").classList.toggle("overflow_hidden");
+  });
+
+  // Hero tab selector on the main page
+  var tab = $(".hero-tab");
+  var content = $(".hero-content");
+
+  tab.on("click", function (event) {
+    var activeContent = $(this).attr("data-target");
+    content.removeClass("hero-content_active");
+    tab.removeClass("hero-tab_active");
+    $(activeContent).addClass("hero-content_active");
+    $(this).addClass("hero-tab_active");
   });
 });
